bc4717ca952e89b114c1fc60ccf4d4fb8f5dd8ab
jaymanion <40965097+jaymanion@users.noreply.github.com>
2024-04-28 10:27:25 -0700

    updating with not null and primary keys

--- EmployeeSQLQuery.sql
+++ EmployeeSQLQuery.sql
@@ -1,36 +1,41 @@
 CREATE TABLE departments (
     dept_no VARCHAR(4) PRIMARY KEY,
-    dept_name VARCHAR(255)
+    dept_name VARCHAR(255) NOT NULL
 );
+
 CREATE TABLE dept_emp (
-    emp_no INT,
-    dept_no VARCHAR(4),
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
+
 CREATE TABLE dept_manager (
-    dept_no VARCHAR(4),
-    emp_no INT,
+    dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
+
 CREATE TABLE employees (
     emp_no INT PRIMARY KEY,
-    emp_title_id VARCHAR(5),
-    birth_date DATE,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    sex CHAR(1),
-    hire_date DATE
+    emp_title_id VARCHAR(5) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    sex CHAR(1) NOT NULL,
+    hire_date DATE NOT NULL
 );
+
 CREATE TABLE salaries (
-    emp_no INT,
-    salary INT,
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
+
 CREATE TABLE titles (
     title_id VARCHAR(5) PRIMARY KEY,
-    title VARCHAR(255)
+    title VARCHAR(255) NOT NULL
 );
 
 COPY departments(dept_no, dept_name)
